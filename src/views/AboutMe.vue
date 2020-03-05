e4236e2bd07b9b971a0ed5eb825b042120d2cfc6
KEN <ken.s.vollyball@gmail.com>
2020-03-05 21:04:15 +0900

    Finish I am page.

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,37 +1,76 @@
 <template>
   <div class="about">
     <h1>Hi..</h1>
-    <h1>Welcome to Ken's page.</h1>
+    <h1>Welcome to Ken's Portfolio Site</h1>
 
     <div class="iam">
-      <p>I am..</p>
+      <p>
+        <i class="far fa-user"></i>
+        I am..
+      </p>
       <table class="about_table">
         <tr>
-          <td>Name</td>
-          <th>Ken Segami</th>
+          <th>Name</th>
+          <td>Ken Segami</td>
         </tr>
 
         <tr>
-          <td>Age</td>
-          <th>23</th>
+          <th>Age</th>
+          <td>23</td>
+        </tr>
+
+        <tr>
+          <th>Address</th>
+          <td>Tokyo, Japan</td>
+        </tr>
+        
+        <tr>
+          <th>University</th>
+          <td>Osaka City University</td>
+        </tr>
+
+        <tr>
+          <th>Link</th>
+          <td>
+            <a href="https://twitter.com/ken_0205_jp" target="_blank">
+              <i class="fab fa-twitter-square"></i>
+            </a>
+            <a href="https://github.com/segamiken" target="_blank">
+              <i class="fab fa-github"></i>
+            </a>
+            <a href="https://qiita.com/ken-s" target="_blank">
+              <i class="fas fa-laptop-code"></i>
+            </a>
+          </td>
+        </tr>
+
+        <tr>
+          <th>Blog</th>
+          <td>
+            <a href="https://ken-japanmalaysia.com" target="_blank">とある大学5年生のブログ</a>
+          </td>
         </tr>
       </table>
     </div>
 
     <div class="career">
-      <p>Career..</p>
+      <p>
+        <i class="fas fa-briefcase"></i>
+        Career..
+      </p>
       <table class="about_table">
         <tr>
-          <td>現在の職業</td>
-          <th>大学4年生</th>
+          <th>Current Job</th>
+          <td>大学4年生</td>
         </tr>
 
         <tr>
-          <td></td>
-          <th>2020年3月卒業予定</th>
+          <th></th>
+          <td>2020年3月卒業予定</td>
         </tr>
       </table>
       <ul>
+        <li></li>
         <li></li>
         <li></li>
       </ul>
@@ -40,6 +79,10 @@
 </template>
 
 <style>
+  a {
+    text-decoration: none;
+  }
+  
   .iam {
     width: 25%;
     background-color:#fff;
@@ -56,12 +99,23 @@
     box-shadow: 0 8px 7px #ddd;
     float: left;
     margin-right: 24%;
-    margin-right: 1%;
+    margin-left: 1%;
     height: 400px;
   }
 
   .about_table {
     margin: 0 auto;
+    text-align: left;
+  }
+
+  .about_table th {
+    line-height: 300%;
+    padding-right: 20px;
+  }
+
+  .about_table i{
+    display: inline-block;
+    margin-right: 20px;
   }
 
 </style>
