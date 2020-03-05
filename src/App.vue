b10e51996a849d3f25bf78b794cc41017a2b6c0a
Ken <ken.s,vollyball@gmail.com>
2020-03-05 16:16:17 +0900

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,12 @@
 <template>
-  <div>
-    <myheader></myheader>
-    <p v-if="msg.length > 0">
-      {{msg}}
-    </p>
-    <p v-else>
-      no text
-    </p>
-    <input type="text" v-model="msg">
-    <button @click="clear()">clear</button>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-  import myheader from './components/myheader'
-  
-  export default {
-  components: {
-    myheader
-  },
-  data () {
-    return {
-      msg: 'Hello World!'
-    }
-  },
-  methods: {
-    clear () {
-      this.msg = ''
-    }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -39,6 +15,18 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
