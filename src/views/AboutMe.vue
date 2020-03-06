18e2bc47d309fb2fd58f5a659033b213c71056ee
KEN <ken.s.vollyball@gmail.com>
2020-03-06 12:20:09 +0900

    Finish Career page.

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="about">
-    <h1>Hi..</h1>
-    <h1>Welcome to Ken's Portfolio Site</h1>
 
+    <!-- 題名 -->
+    <h1>Hi.</h1>
+    <h1>Welcome to Ken's Portfolio Site</h1>
+    <!-- 題名ここまで -->
+
+    <!-- 自己紹介 -->
     <div class="iam">
       <p>
         <i class="far fa-user"></i>
@@ -50,9 +54,17 @@
             <a href="https://ken-japanmalaysia.com" target="_blank">とある大学5年生のブログ</a>
           </td>
         </tr>
+
+        <tr>
+          <th>Contact</th>
+          <td>TwitterのDMでお願い致します。</td>
+        </tr>
       </table>
     </div>
+    <!-- 自己紹介ここまで -->
 
+
+    <!-- 職歴 -->
     <div class="career">
       <p>
         <i class="fas fa-briefcase"></i>
@@ -69,12 +81,41 @@
           <td>2020年3月卒業予定</td>
         </tr>
       </table>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+
+      <ul class="timeline">
+        <li class="timeline-item">
+          <div class="timeline-item__tail"></div>
+          <div class="timeline-item__node"></div>
+          <div class="timeline-item__wrapper">
+            Web広告企業でWebエンジニア
+            <i class="fab fa-laravel"></i>
+            <i class="fab fa-vuejs"></i>
+          </div>
+        </li>
+        <li class="timeline-item">
+          <div class="timeline-item__tail"></div>
+          <div class="timeline-item__node"></div>
+          <div class="timeline-item__wrapper">
+            プログラミングスクールメンター(Rails)
+          </div>
+        </li>
+        <li class="timeline-item">
+          <div class="timeline-item__tail"></div>
+          <div class="timeline-item__node"></div>
+          <div class="timeline-item__wrapper">
+            Sunway University(Malaysia)留学
+          </div>
+        </li>
+        <li class="timeline-item">
+          <div class="timeline-item__tail__last"></div>
+          <div class="timeline-item__node"></div>
+          <div class="timeline-item__wrapper">
+            大阪市立大学 商学部 入学
+          </div>
+        </li>
       </ul>
     </div>
+    <!-- 職歴ここまで -->
   </div>
 </template>
 
@@ -85,22 +126,24 @@
   
   .iam {
     width: 25%;
+    margin-left: 24%;
+    margin-right: 1%;
+    height: 450px;
+
     background-color:#fff;
     box-shadow: 0 8px 7px #ddd;
     float: left;
-    margin-left: 24%;
-    margin-right: 1%;
-    height: 400px;
   }
 
   .career {
     width: 25%;
+    margin-right: 24%;
+    margin-left: 1%;
+    height: 450px;
+
     background-color:#fff;
     box-shadow: 0 8px 7px #ddd;
     float: left;
-    margin-right: 24%;
-    margin-left: 1%;
-    height: 400px;
   }
 
   .about_table {
@@ -113,9 +156,46 @@
     padding-right: 20px;
   }
 
+  /* font-awesome */
   .about_table i{
     display: inline-block;
     margin-right: 20px;
   }
 
+  /* 職歴の系列のデザイン */
+  .timeline {
+    margin-top: 40px;
+    font-size: 14px;
+    list-style: none;
+  }
+
+  .timeline-item {
+    position: relative;
+    padding-bottom: 20px;
+  }
+
+  .timeline-item__tail {
+    position: absolute;
+    border-left: 2px solid #e4e7ed;
+    height: 100%;
+    left: 4px;
+  }
+
+  .timeline-item__node {
+    width: 12px;
+    height: 12px;
+    position: absolute;
+    background-color: #e4e7ed;
+    border-radius: 50%;
+  }
+
+  .timeline-item__wrapper {
+    position: relative;
+    top: -3px;
+  }
+
+   .timeline-item__wrapper i{
+    display: inline-block;
+    margin-left: 5px;
+  }
 </style>
